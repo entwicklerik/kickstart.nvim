87c7dbd7f12836e8863fa322a9ed93528b7aee19
entwicklerik <zians.erik@gmail.com>
2024-05-22 20:04:26 +0200

    Remove mini.git

--- init.lua
+++ init.lua
@@ -737,7 +737,6 @@ require('lazy').setup({
       -- - sr)'  - [S]urround [R]eplace [)] [']
       require('mini.surround').setup()
 
-      require('mini.git').setup()
       -- Simple and easy statusline.
       --  You could remove this setup call if you don't like it,
       --  and try some other statusline plugin
